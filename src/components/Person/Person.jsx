d53cf613300f4f55bbb41fa1c594baaeefb5a0ef
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-09-27 18:15:35 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,26 @@
-// export const Person = ({ person }) => ();
+function doMarried(sex, partner) {
+  if (sex === 'm') {
+    return `${partner} is my wife`;
+  }
+
+  if (sex === 'f') {
+    return `${partner} is my husband`;
+  }
+}
+
+export const Person = ({ person }) => (
+  <div className="App">
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {person.age > 0 && <p className="Person__age">I am {person.age}</p>}
+
+      <p className="Person__partner">
+        {!person.isMarried
+          ? 'I am not married' :
+          doMarried(person.sex, person.partnerName)
+        }
+      </p>
+    </section>
+  </div>
+);
